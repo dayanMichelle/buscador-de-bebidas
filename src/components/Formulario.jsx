82d0df2cbe643313e7f892971079446c1bfe097f
Dayan Arango <dayaara6@gmail.com>
2022-05-10 15:57:05 -0400

    manejo del formulario y alertas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,26 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Row, Col, Alert } from "react-bootstrap";
 import useCategorias from "../hooks/useCategorias";
 const Formulario = () => {
+  const [busqueda, setBusqueda] = useState({
+    nombre: "",
+    categoria: "",
+  });
+  const [error, setError] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(Object.values(busqueda).includes('')){
+        setError('Todos los campos son obligatorios');
+        return
+    }
+    setError('');
+  }
+
   const { categorias } = useCategorias();
-  console.log(categorias)
+  console.log(categorias);
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
+        {error && <Alert className="my-5 text-center" variant="danger">{error}</Alert>}
       <Row>
         <Col md={6}>
           <Form.Group className="mb-3">
@@ -14,15 +30,32 @@ const Formulario = () => {
               type="text"
               placeholder="Tequila, Vodka, etc..."
               name="nombre"
+              value={busqueda.nombre}
+              onChange={(e) =>
+                setBusqueda({
+                  ...busqueda,
+                  [e.target.name]: e.target.value,
+                })
+              }
             />
           </Form.Group>
         </Col>
         <Col md={6}>
           <Form.Group className="mb-3">
             <Form.Label htmlFor="categoria">Categoria Bebida</Form.Label>
-            <Form.Select id="categoria" name="categoria">
+            <Form.Select
+              value={busqueda.categoria}
+              onChange={(e) =>
+                setBusqueda({
+                  ...busqueda,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              id="categoria"
+              name="categoria"
+            >
               <option>-- Selecciona Categoria --</option>
-              {categorias.map(categoria => (
+              {categorias.map((categoria) => (
                 <option
                   key={categoria.strCategory}
                   value={categoria.strCategory}
@@ -33,14 +66,13 @@ const Formulario = () => {
             </Form.Select>
           </Form.Group>
         </Col>
-      </Row>  
+      </Row>
       <Row className="justify-content-end">
-          <Col md={3}>
-              <Button
-              variant="danger"
-              className="text-uppercase w-100"
-              >Buscar Bebidas</Button>
-          </Col>
+        <Col md={3}>
+          <Button variant="danger" type="submit" className="text-uppercase w-100">
+            Buscar Bebidas
+          </Button>
+        </Col>
       </Row>
     </Form>
   );
