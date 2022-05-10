2a94f801426e5e3efe39a8e05edefae1a9ec0da0
Dayan Arango <dayaara6@gmail.com>
2022-05-10 16:35:33 -0400

    obtenido receta segun id

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -2,7 +2,7 @@ import { Card, Col, Button } from "react-bootstrap";
 import useBebidas from "../hooks/useBebidas";
 
 const Bebida = ({ bebida }) => {
-  const { handleModalClick } = useBebidas()
+  const { handleModalClick, handleBebidaId } = useBebidas();
   return (
     <Col md={6} lg={3}>
       <Card className="mb-4">
@@ -14,10 +14,13 @@ const Bebida = ({ bebida }) => {
         <Card.Body>
           <Card.Title>{`Bebida de ${bebida.strDrink}`}</Card.Title>
           <Button
-          onClick={() => 
-            handleModalClick()
-          }
-          className="w-100 text-uppercase mt-2" variant="warning">
+            onClick={() => {
+              handleModalClick();
+              handleBebidaId(bebida.idDrink);
+            }}
+            className="w-100 text-uppercase mt-2"
+            variant="warning"
+          >
             Ver Receta
           </Button>
         </Card.Body>
--- src/context/BebidasProvider.jsx
+++ src/context/BebidasProvider.jsx
@@ -5,8 +5,24 @@ import { createContext, useEffect, useState } from "react";
 const BebidasContext = createContext();
 
 const BebidasProvider = ({ children }) => {
+    const [ bedidaId, setBebidaId ] = useState(null);
     const [bebidas, setBebidas] = useState([]);
     const [modal, setModal] = useState(false);
+    const [ receta, setReceta ] = useState([]);
+
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!bedidaId) return;
+            try {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${bedidaId}`;
+                const { data } = await axios.get(url);
+                setReceta(data.drinks[0]);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerReceta()
+    }, [bedidaId]);
 
     const consultarBebidas = async (datos) => {
         try {
@@ -23,13 +39,18 @@ const BebidasProvider = ({ children }) => {
         setModal(!modal);
     }
 
+    const handleBebidaId = (id) => {
+        setBebidaId(id);
+    }
   
     return (
         <BebidasContext.Provider value={{
          consultarBebidas,
          bebidas,
          handleModalClick,
-         modal
+         modal,
+         handleBebidaId,
+         receta
         }}>
             {children}
         </BebidasContext.Provider>
