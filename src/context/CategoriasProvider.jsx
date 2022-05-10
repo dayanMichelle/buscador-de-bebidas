dd0f5f19a9acb7d79ce5f4712e6bae0137f02c73
Dayan Arango <dayaara6@gmail.com>
2022-05-10 15:41:41 -0400

    obteniendo la infromacion de las bebidas

--- src/context/CategoriasProvider.jsx
+++ src/context/CategoriasProvider.jsx
@@ -1,11 +1,27 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 
 const CategoriasContext = createContext();
 
 const CategoriasProvider = ({ children }) => {
+    const [categorias, setCategorias] = useState([]);
+    const obtenerCategorias = async () => {
+        try {
+            const url ='https://www.thecocktaildb.com/api/json/v1/1/list.php?category=list';
+            const {data} = await axios.get(url);
+            setCategorias(data.drinks);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect (() => {
+        obtenerCategorias()
+    },[])
     return (
-        <CategoriasContext.Provider value={{}}>
+        <CategoriasContext.Provider value={{
+            categorias
+        }}>
             {children}
         </CategoriasContext.Provider>
     )
